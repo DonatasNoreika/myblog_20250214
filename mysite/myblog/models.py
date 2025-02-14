521a1bfd559304a7e75aea91a6c9b7d4b48b2aa0
DonatasNoreika <donoras@gmail.com>
2025-02-14 09:23:29 +0200

    +Vieno įrašo puslapyje atvaizduojami komentarai

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -17,4 +17,4 @@ class Comment(models.Model):
     content = models.TextField(verbose_name="Content", max_length=3000)
     date_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
     author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.CASCADE)
-    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE, related_name="comments")
