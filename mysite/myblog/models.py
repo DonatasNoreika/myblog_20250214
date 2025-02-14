a9d8bc409145a91d6b35b511936be90a7eb4656e
DonatasNoreika <donoras@gmail.com>
2025-02-14 08:25:01 +0200

    +Modelių aprašymas

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=20)
+    content = models.TextField(verbose_name="Content", max_length=5000)
+    date_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
+    author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField(verbose_name="Content", max_length=3000)
+    date_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
+    author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myblog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,11 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Vilnius'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
