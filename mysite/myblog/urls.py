029574ba6261aa848844634d2b0d8ad140de999f
DonatasNoreika <donoras@gmail.com>
2025-02-14 08:57:39 +0200

    +Vieno įrašo puslapis

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="posts"),
+    path("posts/<int:pk>", PostDetailView.as_view(), name="post"),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -8,3 +8,9 @@ class PostListView(generic.ListView):
     model = Post
     template_name = "posts.html"
     context_object_name = "posts"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "post.html"
+    context_object_name = "post"
