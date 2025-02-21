50b5570400217268c41baa367307981a8c56b2e2
DonatasNoreika <donoras@gmail.com>
2025-02-21 08:27:35 +0200

    +Padaryti, kad įrašo tekstas būtų HTML

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(verbose_name="Title", max_length=20)
-    content = models.TextField(verbose_name="Content", max_length=5000)
+    content = HTMLField(verbose_name="Content", max_length=5000)
     date_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
     author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.CASCADE)
 
